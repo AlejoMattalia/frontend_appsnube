b6a8039af7ed14b8bb2eba0821467604f9f997eb
Alejo Mattalia <aleacade.mattalia@gmail.com>
2024-10-18 14:48:50 -0300

    Solucion token

--- src/app/hook/useClearToken.tsx
+++ src/app/hook/useClearToken.tsx
@@ -6,14 +6,23 @@ import Cookies from 'js-cookie';
 export const useClearToken = () => {
   useEffect(() => {
     const handleBeforeUnload = () => {
-      // Aquí puedes definir la lógica de limpieza
+      // Elimina el token cuando se cierra la pestaña o se actualiza la página
       Cookies.remove('token');
     };
 
+    const handleVisibilityChange = () => {
+      // Elimina el token cuando se cambia a otra pestaña o ventana
+      if (document.visibilityState === 'hidden') {
+        Cookies.remove('token');
+      }
+    };
+
     window.addEventListener('beforeunload', handleBeforeUnload);
+    document.addEventListener('visibilitychange', handleVisibilityChange);
 
     return () => {
       window.removeEventListener('beforeunload', handleBeforeUnload);
+      document.removeEventListener('visibilitychange', handleVisibilityChange);
     };
   }, []);
 };
